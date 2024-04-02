f4b96cdf87fb9f676db5f8a3ca13e50103878bf8
Martin Chavez <martinchavez1203@gmail.com>
2024-04-01 18:44:46 -0600

    test for validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,20 @@ RSpec.describe User, type: :model do
     it { should_not allow_value('something.something@').for(:email) }
     it { should_not allow_value('something').for(:email) }
 
+
+    it { should validate_uniqueness_of(:email)}
+    it { should validate_presence_of(:password)}
+    it { should have_secure_password}
+    
+    # it { should validate_presence_of(:password_digest)}
+    #should i include this? ^^^
+  end
+  
+  it 'should not show plain password' do
+    user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq('password123')
+    
   end
 
   describe 'associations' do
